b419eb8c48eaa824c3fa50e701fb8025344b6dc3
Anya Kapoor <58616240+anyakapoor6@users.noreply.github.com>
2025-05-29 12:47:52 -0700

    Add pet likes functionality to likes store

--- store/likes.ts
+++ store/likes.ts
@@ -3,15 +3,19 @@ import { create } from 'zustand';
 interface LikesState {
   storyLikes: Set<string>;
   commentLikes: Set<string>;
+  petLikes: Set<string>;
   toggleStoryLike: (storyId: string) => void;
   toggleCommentLike: (commentId: string) => void;
+  togglePetLike: (petId: string) => void;
   isStoryLiked: (storyId: string) => boolean;
   isCommentLiked: (commentId: string) => boolean;
+  isPetLiked: (petId: string) => boolean;
 }
 
 export const useLikes = create<LikesState>((set, get) => ({
   storyLikes: new Set(),
   commentLikes: new Set(),
+  petLikes: new Set(),
   
   toggleStoryLike: (storyId: string) => {
     set(state => {
@@ -36,6 +40,18 @@ export const useLikes = create<LikesState>((set, get) => ({
       return { commentLikes: newLikes };
     });
   },
+
+  togglePetLike: (petId: string) => {
+    set(state => {
+      const newLikes = new Set(state.petLikes);
+      if (newLikes.has(petId)) {
+        newLikes.delete(petId);
+      } else {
+        newLikes.add(petId);
+      }
+      return { petLikes: newLikes };
+    });
+  },
   
   isStoryLiked: (storyId: string) => {
     return get().storyLikes.has(storyId);
@@ -43,5 +59,9 @@ export const useLikes = create<LikesState>((set, get) => ({
   
   isCommentLiked: (commentId: string) => {
     return get().commentLikes.has(commentId);
+  },
+
+  isPetLiked: (petId: string) => {
+    return get().petLikes.has(petId);
   },
 }));
