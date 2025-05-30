774717abef67b0c6e4a34eec22ed4c3974a2602f
Anya Kapoor <58616240+anyakapoor6@users.noreply.github.com>
2025-05-30 02:57:11 -0700

    Enable package exports in Metro config

--- metro.config.cjs
+++ metro.config.cjs
@@ -1,4 +1,4 @@
 const { getDefaultConfig } = require('expo/metro-config');
 const config = getDefaultConfig(__dirname);
-config.resolver.unstable_enablePackageExports = false;
+config.resolver.unstable_enablePackageExports = true;
 module.exports = config;
